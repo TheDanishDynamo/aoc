6fc22ea7cbebd905b6c269633cb230d473ee9ef8
Thomas Ulrich <thomas@code-sharp.com>
2021-12-02 14:53:54 -0500

    aoc dec 1 part 1

---
+++ aoc-2021/dec-01-part-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+// https://adventofcode.com/2021/day/1
+namespace dec_01_part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@"puzzle-input.txt");
+            int[] reading = input.Select(int.Parse).ToArray();
+            Console.WriteLine($"Read {reading.Length} values");
+            int x = 0;
+            for (int i = 1; i < reading.Length; i++)
+            {
+                if (reading[i] - reading[i - 1] > 0)
+                {
+                    x++;
+                }
+            }
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
